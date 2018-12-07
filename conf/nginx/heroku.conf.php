86540ffb90a3393ae4e6c46a0e29b77b583416a1
e.shershnov <e.shershnov@nodeart.io>
2018-12-07 16:41:14 +0200

    Increased request limit

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -1,5 +1,5 @@
 http {
-    limit_req_zone $binary_remote_addr zone=test:10m rate=3r/s;
+    limit_req_zone $binary_remote_addr zone=test:10m rate=5r/s;
 	include       mime.types;
 	default_type  application/octet-stream;
 
