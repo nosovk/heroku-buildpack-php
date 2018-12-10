a645ebf425b9cc17858422e96f9152b0af11b416
Konstantin Nosov <nosovk@gmail.com>
2018-12-10 20:30:24 +0200

    Add debug header

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -68,6 +68,7 @@ http {
 
 		# default handling of .php
 		location ~ \.php {
+			add_header X-debug-enabled "true" always;
 			add_header X-debug-message $http_x_forwarded_for always;
                         add_header X-debug-message-cloudflare $http_cf_connecting_ip always;
 			try_files @heroku-fcgi @heroku-fcgi;
